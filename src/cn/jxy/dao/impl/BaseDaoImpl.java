7492bae468bb491785d098f64ed00751a0843a34
Administrator <574231288@qq.com>
2018-01-16 17:05:53 +0800

    08_面向对象之继承优化JDBC代码(此版本为准,前版本部分代码漏提交)   1:采用Java继承的方式来实现update、insert、delete的抽取

--- src/cn/jxy/dao/impl/BaseDaoImpl.java
+++ src/cn/jxy/dao/impl/BaseDaoImpl.java
@@ -8,36 +8,7 @@ import java.sql.SQLException;
 import cn.jxy.model.Product;
 import cn.jxy.utils.JdbcUtils;
 
-public abstract class BaseDaoImpl<T> {
-
-	// 由于不同的子类,传入的sql语句不同.返回的结果集肯定不同,代码不具备共性.因此此代码父类不能抽取
-	protected abstract T getRow(ResultSet rs);
-
-	// T java中的泛型,此类型可以在具体的子类来指定
-	protected T getById(String sql, Object id) {
-		T model = null;
-		// 1: 获取connection连接对象
-		Connection conn = null;
-		PreparedStatement pre = null;
-		ResultSet rs = null;
-		try {
-			// 2: 给参数赋值,下标从1开始
-			conn = JdbcUtils.getConnection();
-			pre = conn.prepareStatement(sql);
-			pre.setObject(1, id);
-			// 查询返回的是结果集
-			rs = pre.executeQuery();
-			if (rs.next()) { // 当前行存在记录.DB中的一条记录对应Java中的一个对象
-				model = this.getRow(rs);
-			}
-			return model;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			// 4： 释放资源
-			JdbcUtils.close(rs, pre, conn);
-		}
-	}
+public class BaseDaoImpl {
 
 	// update方法用来操作 delete、update、insert
 	protected int update(String sql, Object[] param) {
--- src/cn/jxy/dao/impl/ProductDaoImpl.java
+++ src/cn/jxy/dao/impl/ProductDaoImpl.java
@@ -8,12 +8,12 @@ import java.sql.SQLException;
 import cn.jxy.model.Product;
 import cn.jxy.utils.JdbcUtils;
 
-public class ProductDaoImpl {
-	
+public class ProductDaoImpl extends BaseDaoImpl {
+
 	// main测试缺点: 不能并行测试, 有侵入性
-//	public static void main(String[] args) {
-//	
-//	}
+	// public static void main(String[] args) {
+	//
+	// }
 
 	public Product getById(int id) {
 		Product product = null;
@@ -29,7 +29,7 @@ public class ProductDaoImpl {
 			pre.setInt(1, id);
 			// 查询返回的是结果集
 			rs = pre.executeQuery();
-			if(rs.next()){ // 当前行存在记录.DB中的一条记录对应Java中的一个对象
+			if (rs.next()) { // 当前行存在记录.DB中的一条记录对应Java中的一个对象
 				product = new Product();
 				product.setId(rs.getInt("id"));
 				product.setName(rs.getString("name"));
@@ -41,75 +41,31 @@ public class ProductDaoImpl {
 			throw new RuntimeException(e);
 		} finally {
 			// 4： 释放资源
-			JdbcUtils.close(rs,pre, conn);
+			JdbcUtils.close(rs, pre, conn);
 		}
 	}
 
 	// 数据入库操作
 	public int save(Product product) {
 		String sql = "insert into product (name,price,remark) values (?,?,?)";
-		// 1: 获取connection连接对象
-		Connection conn = null;
-		PreparedStatement pre = null;
-		try {
-			// 2: 给参数赋值,下标从1开始
-			conn = JdbcUtils.getConnection();
-			pre = conn.prepareStatement(sql);
-			pre.setString(1, product.getName());
-			pre.setDouble(2, product.getPrice());
-			pre.setString(3, product.getRemark());
-			// 3: 真正执行sql语句,executeUpdate:负责update、delete、save操作
-			return pre.executeUpdate();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			// 4： 释放资源
-			JdbcUtils.close(pre, conn);
-		}
+		return super.update(
+				sql,
+				new Object[] { product.getName(), product.getPrice(),
+						product.getRemark() });
 	}
 
 	// 数据更新操作
 	public int update(Product product) {
 		String sql = "update product set name=?,price=?,remark=? where id=?";
-		// 1: 获取connection连接对象
-		Connection conn = null;
-		PreparedStatement pre = null;
-		try {
-			// 2: 给参数赋值,下标从1开始
-			conn = JdbcUtils.getConnection();
-			pre = conn.prepareStatement(sql);
-			pre.setString(1, product.getName());
-			pre.setDouble(2, product.getPrice());
-			pre.setString(3, product.getRemark());
-			pre.setInt(4, product.getId());
-			// 3: 真正执行sql语句
-			return pre.executeUpdate();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			// 4： 释放资源
-			JdbcUtils.close(pre, conn);
-		}
+		return super.update(
+				sql,
+				new Object[] { product.getName(), product.getPrice(),
+						product.getRemark(), product.getId() });
 	}
 
 	// 数据删除操作
 	public int delete(int id) {
 		String sql = "delete from product where id=?";
-		// 1: 获取connection连接对象
-		Connection conn = null;
-		PreparedStatement pre = null;
-		try {
-			// 2: 给参数赋值,下标从1开始
-			conn = JdbcUtils.getConnection();
-			pre = conn.prepareStatement(sql);
-			pre.setInt(1, id);
-			// 3: 真正执行sql语句
-			return pre.executeUpdate();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			// 4： 释放资源
-			JdbcUtils.close(pre, conn);
-		}
+		return super.update(sql, new Object[] { id });
 	}
 }
